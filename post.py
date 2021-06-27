27b333563a59c80d9990c44df02fc7e93add0d73
lester9991 <81787272+lester9991@users.noreply.github.com>
2021-06-27 22:00:47 +0530

    Initial Commit

--- post.py
+++ post.py
@@ -2,11 +2,11 @@ import smtplib
 import os
 
 
-def send_email(items, email="lesterdsouza820@gmail.com"):
+def send_email(items):
     with smtplib.SMTP(os.environ['SMTP'], port=587) as connection:
         connection.starttls()
         connection.login(os.environ['EMAIL'], os.environ['PASS'])
-        connection.sendmail(os.environ['EMAIL'], email,
+        connection.sendmail(os.environ['EMAIL'], os.environ['ADMIN'],
                             f"Subject:MESSAGE\n\n"
                             f"Name: {items['name']}\nEmail: {items['email']}\n"
                             f"Phone: {items['phone-number']}\nMessage: {items['textarea']}".encode("utf-8"))
